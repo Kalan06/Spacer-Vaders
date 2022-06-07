f4913f2b727dee2deb6ad4973789613cf62ccf9c
Kalan Klaus-Morunga <106497919+Kalan06@users.noreply.github.com>
2022-06-07 00:25:21 +0000

    k

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -11,14 +13,24 @@ def run_game():
   screen = pygame.display.set_mode(
     (ai_settings.screen_width, ai_settings.screen_height))
   pygame.display.set_caption("Alien Invasion")
-
-  ship = Ship(screen)
+  # Make an alien.
+  alien = Alien(ai_settings, screen)
+  ship = Ship(ai_settings, screen)
+  # Make a group to store bullets in.
+  bullets = Group()
   bg_color = (ai_settings.bg_color)
 
   while True:
-    gf.check_events(ship)
+    gf.check_events(ai_settings, screen, ship, bullets)
     ship.update()
-    gf.update_screen(ai_settings, screen, ship)
+    gf.update_bullets(bullets)
+    gf.update_screen(ai_settings, screen, ship, alien, bullets)
+
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+    	if bullet.rect.bottom <= 0:
+    		bullets.remove(bullet)
+    print(len(bullets))
 
 
 run_game()
--- settings.py
+++ settings.py
@@ -3,6 +3,14 @@ class Settings():
  def __init__(self):
    """Initialize the game's settings."""
    # Screen settings
-   self.screen_width = 600
+   self.screen_width = 1200
    self.screen_height = 400
    self.bg_color = (22, 1, 48)
+   # Ship settings
+   self.ship_speed_factor = 0.5
+   # Bullet settings
+   self.bullet_speed_factor = 1
+   self.bullet_width = 3
+   self.bullet_height = 15
+   self.bullet_color = 60, 60, 60
+   self.bullets_allowed = 3
